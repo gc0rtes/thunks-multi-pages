8aaeefc9a77ad44eb96252ac0060a0428b49d31a
gc0rtes <guilhermeco@gmail.com>
2021-05-30 11:40:05 +0200

    Start Creating a LoginPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Switch>
         {/* more pages to be added here later */}
         <Route path="/post/:id" component={PostPage} />
+        <Route path="/login" component={LoginPage} />
         <Route component={HomePage} />
       </Switch>
     </div>
